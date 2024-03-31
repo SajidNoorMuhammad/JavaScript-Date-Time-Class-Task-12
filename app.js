53da4b516814b558372c7033452f97c77b295838
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-03-31 15:59:40 +0500

    Add 2nd Variable.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // var dob= prompt("Enter Your Birth Year")
 
 var birthYear= new Date ('28 june, 2005').getTime()
-// var todaysDate= new Date ().getTime()
+var todaysDate= new Date ().getTime()
 // var minusDate= todaysDate - birthYear
 // var convertAge= minusDate / (1000 * 60 * 60 * 24 * 365)
 
