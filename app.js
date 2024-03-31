4d191e1ce0ca8b7a3b77a7373623f7ff0d84cac4
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-03-31 16:00:41 +0500

    Add Formula.

--- app.js
+++ app.js
@@ -3,6 +3,6 @@
 var birthYear= new Date ('28 june, 2005').getTime()
 var todaysDate= new Date ().getTime()
 var minusDate= todaysDate - birthYear
-// var convertAge= minusDate / (1000 * 60 * 60 * 24 * 365)
+var convertAge= minusDate / (1000 * 60 * 60 * 24 * 365)
 
 // document.write (Math.floor(convertAge))
